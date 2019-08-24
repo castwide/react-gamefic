faeb99fa1cd0a7ebaf9531a26151be0f46478e28
Fred Snyder <fsnyder@castwide.com>
2019-08-24 08:51:02 -0400

    CommandLink uses children instead of text attribute.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.1 - August 24, 2019
+- `CommandLink` uses children instead of text attribute
+
 ## 0.1.0
 - `Console` supports custom UI components
 - Default `Terminal` UI
--- src/CommandLink/CommandLink.jsx
+++ src/CommandLink/CommandLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export class CommandLink extends React.Component {
 	render() {
 		return (
-			<a className={'CommandLink' + (this.props.disabled ? ' disabled' : '')} href="#" data-command={this.props.command}>{this.props.text || this.props.command}</a>
+			<a className={'CommandLink' + (this.props.disabled ? ' disabled' : '')} href="#" data-command={this.props.command}>{this.props.children || this.props.command}</a>
 		);
 	}
 }
