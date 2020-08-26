cf632d768d796b7ae63595fab13e06a9b09334c5
Fred Snyder <fsnyder@castwide.com>
2020-08-26 10:28:28 -0400

    MultipleChoiceScene renders options.

--- src/MultipleChoiceScene/MultipleChoiceScene.jsx
+++ src/MultipleChoiceScene/MultipleChoiceScene.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import {Output} from '../Output';
 import {CommandLink} from '../CommandLink';
-import {CommandForm} from '../CommandForm';
 
 export class MultipleChoiceScene extends React.Component {
+	renderChoices() {
+		if (this.props.state.options) {
+			const listItems = this.props.state.options.map((opt, index) =>
+				<li key={index}>
+					<CommandLink command={opt}>{opt}</CommandLink>
+				</li>
+			);
+			return (
+				<nav>
+					{this.props.state.options}
+					<ol>
+						{listItems}
+					</ol>
+				</nav>
+			);
+		} else {
+			console.warn("Error: Multiple choice scene does not have any options");
+		}
+	}
+
 	render() {
 		return (
 			<div className="MultipleChoiceScene">
 				{React.createElement(this.props.outputComponent, this.props, null)}
-				<CommandForm prompt={this.props.state.prompt} />
+				<label>{this.props.state.prompt}</label>
+				{this.renderChoices()}
 			</div>
 		);
 	}
