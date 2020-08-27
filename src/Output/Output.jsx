ecb284570115c0ca14ac937c056044c5437501c4
Fred Snyder <fsnyder@castwide.com>
2020-08-27 06:31:08 -0400

    Output history attribute

--- src/Output/Output.jsx
+++ src/Output/Output.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import {History} from '../History';
 import {Turn} from '../Turn';
+import PropTypes from 'prop-types';
 
 export class Output extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
+	renderHistory() {
+		if (this.props.history) {
+			return <History {...this.props} />;
+		}
+	}
+
 	render() {
 		return (
 			<div className="Output">
-				<History {...this.props} />
+				{this.renderHistory()}
 				<Turn state={this.props.state} handleCommand={this.props.handleCommand} time="Present" />
 			</div>
 		)
 	}
+}
+
+Output.defaultProps = {
+	history: true
+};
+
+Output.propTypes = {
+	history: PropTypes.bool
 }
