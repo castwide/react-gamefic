c903cd13074f1ac8c1bb6f8acfa1a925e60826b2
Fred Snyder <fsnyder@castwide.com>
2020-08-27 08:28:18 -0400

    Export all components.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import {CommandForm} from './CommandForm';
-import {CommandLink} from './CommandLink';
-import {Console} from './Console';
-import {Terminal} from './Terminal';
-import {History} from './History';
-import {MultipleChoiceScene} from './MultipleChoiceScene';
-import {Output} from './Output';
-import {PauseScene} from './PauseScene';
-import {Turn} from './Turn';
+import { ActivityScene } from './ActivityScene';
+import { CommandForm } from './CommandForm';
+import { CommandLink } from './CommandLink';
+import { ConclusionScene } from './ConclusionScene';
+import { Console } from './Console';
+import { History } from './History';
+import { MultipleChoiceScene } from './MultipleChoiceScene';
+import { Output } from './Output';
+import { PauseScene } from './PauseScene';
+import { Terminal } from './Terminal';
+import { Turn } from './Turn';
 
 export {
-	Console,
-	Terminal,
+	ActivityScene,
 	CommandForm,
 	CommandLink,
+	ConclusionScene,
+	Console,
 	History,
 	MultipleChoiceScene,
 	Output,
 	PauseScene,
+	Terminal,
 	Turn
 };
