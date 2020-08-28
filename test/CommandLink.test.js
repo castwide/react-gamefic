7c37784bab686de1f163c9ef65c38b7ddda445b1
Fred Snyder <fsnyder@castwide.com>
2020-08-28 03:19:48 -0400

    Test CommandLink clicks.

--- test/CommandLink.test.js
+++ test/CommandLink.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { CommandLink } from '../src/CommandLink';
+import { Console } from '../src/Console';
 
 configure({ adapter: new Adapter() });
 
@@ -10,5 +11,21 @@ describe('<CommandLink />', () => {
 	it('renders a link', () => {
 		let link = shallow(<CommandLink command="do">do</CommandLink>);
 		expect(link.find('a').length).toBe(1);
-	})
+	});
+
+	it('responds to a click', () => {
+        let driver = {};
+		let received = null;
+        driver.receive = jest.fn((message) => {
+			received = message;
+		});
+		driver.onUpdate = jest.fn();
+		let console = mount(
+			<Console driver={driver}>
+				<CommandLink command="success">Click Me</CommandLink>
+			</Console>
+		);
+		console.find(CommandLink).simulate('click');
+		expect(received).toEqual("success");
+	});
 });
