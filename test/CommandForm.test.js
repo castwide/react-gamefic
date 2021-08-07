626b0dc04aa8fb1618751d3160421137ebe8b0d4
Fred Snyder <fsnyder@castwide.com>
2021-08-07 17:08:21 -0400

    Turn and CommandForm tests

--- test/CommandForm.test.js
+++ test/CommandForm.test.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import ReactTestUtils from 'react-dom/test-utils';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { CommandForm } from '../src/CommandForm';
+import { Console } from '../src/Console';
+import { expect, it, jest } from '@jest/globals';
 
 configure({ adapter: new Adapter() });
 
 describe('<CommandForm />', () => {
     it('renders a form', () => {
-        let link = mount(<CommandForm />);
-        expect(link.find('form').length).toBe(1);
+        let form = mount(<CommandForm />);
+        expect(form.find('form').length).toBe(1);
+    });
+
+    it('passes command submissions to the console', () => {
+        let received = '';
+        let driver = {
+            onUpdate: jest.fn(() => {
+                return;
+            }),
+            receive: jest.fn((data) => {
+                received = data;
+            })
+        };
+        let cons = ReactTestUtils.renderIntoDocument(
+            <Console driver={driver}>
+                <CommandForm />
+            </Console>
+        );
+        let form = ReactTestUtils.findRenderedComponentWithType(cons, CommandForm);
+        form.textInput.value = 'test';
+        ReactTestUtils.Simulate.change(form.textInput);
+        ReactTestUtils.Simulate.submit(form.formElement);
+        expect(received).toBe('test');
     });
 });
--- test/Turn.test.js
+++ test/Turn.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { configure, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { Turn } from '../src/Turn';
 
 configure({ adapter: new Adapter() });
+
+let container;
 
 describe('<Turn />', () => {
     it('renders messages', () => {
@@ -16,5 +20,61 @@ describe('<Turn />', () => {
         }
         let element = mount(<Turn {...props} />);
         expect(element.html()).toContain(message);
-    })
+    });
+
+    describe('with command links', () => {
+        beforeEach(() => {
+            container = document.createElement('div');
+            document.body.appendChild(container);
+        });
+
+        afterEach(() => {
+            document.body.removeChild(container);
+            container = null;
+        });
+
+        it('handles links in Present', () => {
+            let message = '<p><a href="#" class="gamefic-command" data-command="test"></a></p>';
+            let received = '';
+            let props = {
+                time: 'Present',
+                handleCommand: (cmd) => {
+                    received = cmd;
+                },
+                state: {
+                    messages: message
+                }
+            }
+            act(() => {
+                ReactDOM.render(<Turn {...props} />, container);
+            });
+            let a = container.querySelector('.gamefic-command');
+            act(() => {
+                a.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+            });
+            expect(received).toBe('test');
+        });
+
+        it('disables links in Past', () => {
+            let message = '<p><a href="#" class="gamefic-command" data-command="test"></a></p>';
+            let received = '';
+            let props = {
+                time: 'Past',
+                handleCommand: (cmd) => {
+                    received = cmd;
+                },
+                state: {
+                    messages: message
+                }
+            }
+            act(() => {
+                ReactDOM.render(<Turn {...props} />, container);
+            });
+            let a = container.querySelector('.gamefic-command');
+            act(() => {
+                a.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+            });
+            expect(received).toBe('');
+        });
+    });
 });
