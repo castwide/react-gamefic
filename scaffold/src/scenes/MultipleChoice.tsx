43bcd221393122bc6c6d02f0c61cf50124fe0b84
Fred Snyder <fsnyder@castwide.com>
2024-12-02 08:03:14 -0500

    MultipleChoice key events

--- scaffold/src/scenes/MultipleChoice.tsx
+++ scaffold/src/scenes/MultipleChoice.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { History, Turn, CommandForm, ScenePropsType } from "react-gamefic";
+import React, { useEffect, useRef } from 'react';
+import { History, Turn, ScenePropsType } from 'react-gamefic';
 
-export default function MultipleChoice({
-  output,
-  history,
-  handleInput,
-}: ScenePropsType) {
+export default function MultipleChoice({ output, history, handleInput }: ScenePropsType) {
+  const sceneRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let button: HTMLElement | null = null;
+
+    const handleKey = (event: KeyboardEvent) => {
+      const buttons = sceneRef.current?.querySelectorAll<HTMLElement>('ol li button');
+      if (event.key.match(/[1-9]/)) {
+        const selected = parseInt(event.key);
+        button?.blur();
+        button = buttons?.item(selected - 1) || null;
+        button?.focus();
+      } else if (event.code == 'Escape') {
+        buttons?.forEach((btn) => btn.blur());
+      }
+    }
+    window.addEventListener('keydown', handleKey);
+
+    return () => {
+      window.removeEventListener('keydown', handleKey);
+    }
+  });
+
   return (
-    <div>
+    <div ref={sceneRef}>
       <History turns={history} />
       <Turn output={output} handleInput={handleInput} />
-      <CommandForm prompt={output.prompt} handleInput={handleInput} />
     </div>
   );
 }
