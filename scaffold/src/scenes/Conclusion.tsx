9acaa84fb1e61e526f82d2cb5a9c984e69d27f06
Fred Snyder <fsnyder@castwide.com>
2025-05-31 13:41:55 -0400

    Undo/restore/restart in Conclusion

--- scaffold/src/scenes/Conclusion.tsx
+++ scaffold/src/scenes/Conclusion.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { History, Turn, ScenePropsType } from "react-gamefic";
+import { History, Turn, ScenePropsType, CommandLink } from "react-gamefic";
 
 export default function Conclusion({ output, history }: ScenePropsType) {
   return (
     <div>
       <History turns={history} />
       <Turn output={output} />
+      <nav>
+        <ul>
+          <li>
+            <CommandLink command="Undo">Undo the last action</CommandLink>
+          </li>
+          <li>
+            <CommandLink command="Restore">Restore a saved game</CommandLink>
+          </li>
+          <li>
+            <CommandLink command="Restart">Start a new game</CommandLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
